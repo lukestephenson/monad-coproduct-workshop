cfd6729387a201e0f4f3bdbff933ee43dcf02e46
luke.stephenson <luke.stephenson@rea-group.com>
2016-08-24 16:56:41 +1000

    Tidy up initial solution, make it easier to follow

---
+++ src/main/scala/app/FreeMonadExample.scala
@@ -0,0 +1,33 @@
+package app
+
+import cats.std.vector._
+import cats.syntax.traverse._
+import demo.AppAction.AppActionMonadic
+import demo.SocialNetworkAction._
+import model.{Handle, Tweet}
+
+import scala.concurrent.duration._
+
+object FreeMonadExample {
+
+  def findMostInfluentialAccount(handle1: Handle, handle2: Handle): AppActionMonadic[Handle] = {
+    for {
+      handle1Followers <- getFollowers(handle1)
+      handle2Followers <- getFollowers(handle2)
+      handle1ActiveFollowers <- getUsersActiveTweetCount(handle1Followers)
+      handle2ActiveFollowers <- getUsersActiveTweetCount(handle2Followers)
+      mostActive = if (handle1ActiveFollowers > handle2ActiveFollowers) handle1 else handle2
+    } yield mostActive
+  }
+
+  def getUsersActiveTweetCount(users: Vector[Handle]): AppActionMonadic[Int] =
+    users.traverse[AppActionMonadic, Tweet] { handle =>
+      getMostRecentTweet(handle)
+    }.map(calculateActiveCount)
+
+
+  def calculateActiveCount(followerMostRecentTweets: Vector[Tweet]): Int =
+    followerMostRecentTweets.count { tweet =>
+      tweet.timestamp > System.currentTimeMillis() - 7.days.toMillis
+    }
+}
--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,13 +1,8 @@
 package app
 
-import cats.std.vector._
-import cats.syntax.traverse._
-import demo.AppAction.AppActionMonadic
 import demo.ConfigAction._
-import demo.SocialNetworkAction._
 import demo.{ConfigActionInterpreter, SocialNetworkActionInterpreter, TaskInterpreter}
-import model.{Handle, Tweet}
-import monix.reactive.MulticastStrategy.Async
+import model.Handle
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -35,24 +30,8 @@ object Main {
     val composeHandle = Handle("compose")
     for {
       baseUrl <- getConfig("appUrl")
-      lukeFollowers <- getFollowersWithRecentTweets(lukeHandle)
-      composeFpFollowers <- getFollowersWithRecentTweets(composeHandle)
-      mostActive = if (lukeFollowers > composeFpFollowers) lukeHandle else composeHandle
+      mostActive <- FreeMonadExample.findMostInfluentialAccount(lukeHandle, composeHandle)
     } yield s"$baseUrl/details/${mostActive.handle}"
   }
 
-  def getFollowersWithRecentTweets(handle: Handle): AppActionMonadic[Int] = {
-    val followers: AppActionMonadic[Vector[Handle]] = getFollowers(handle)
-
-    followers.flatMap { handles =>
-      val followerRecentTweet: Vector[AppActionMonadic[Tweet]] = handles.map(getMostRecentTweet)
-      val m: AppActionMonadic[Vector[Tweet]] = followerRecentTweet.sequence
-      m.map { followerTweets =>
-        // TODO consider the time
-        followerTweets
-          .filter(_.timestamp > System.currentTimeMillis() - 7.days.toMillis)
-          .size
-      }
-    }
-  }
 }
